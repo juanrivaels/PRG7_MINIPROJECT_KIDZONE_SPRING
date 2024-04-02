c6bc9e28bea852255c94562adfbebf80fbd7b671
juanrivaels <juanrivaels@gmail.com>
2024-04-02 14:58:12 +0700

    MINI PROJECT

---
+++ src/main/java/id/co/prg7_miniproject_kidzone_spring/Prg7MiniprojectKidzoneSpringApplication.java
@@ -0,0 +1,13 @@
+package id.co.prg7_miniproject_kidzone_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prg7MiniprojectKidzoneSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prg7MiniprojectKidzoneSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/co/prg7_miniproject_kidzone_spring/Prg7MiniprojectKidzoneSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.prg7_miniproject_kidzone_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prg7MiniprojectKidzoneSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
